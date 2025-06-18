133768c32697457e0a36a01a0b105e78f0ca66a2
zmas云科技 <3675239063@qq.com>
2025-06-18 22:42:57 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,10 +10,11 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: '如何写插件',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: '如何写0.1插件', slug: 'guides/example' },
+						{ label: '关于v0.2', slug: 'guides/example2' },
 					],
 				},
 				{
